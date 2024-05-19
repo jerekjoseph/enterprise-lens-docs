d1db76329e952c90a1ece4b92d91429123e91854
MCN <60007795+jerekjoseph@users.noreply.github.com>
2024-05-19 16:13:49 +0800

    Try version 1 of plugin

--- src/remark/imagesSrcWithBase.js
+++ src/remark/imagesSrcWithBase.js
@@ -2,17 +2,17 @@ const visit = require('unist-util-visit');
 
 const plugin = (options) => {
 /**
-   * Docusaurus does not process JSX `<img src ="...">` URLs
-   * This plugin rewrites the src attribute to `src={require("...").default}`
-   * Markdown links `[]()`, images `![](/image)` and anchor `<a href="...">`
-   * are already automatically processed
-   */
-  const remarkImageSrcWithRequire = (options) => {
+ * Docusaurus does not process JSX `<img src ="...">` URLs
+ * This plugin rewrites the URLs to be relative to `baseUrl`
+ * Markdown links `[]()`, images `![](/image)` and anchor `<a href="...">`
+ * are already automatically processed
+ */
+  const remarkImageSrcWithBase = (options) => {
     return (ast) => {
       visit(ast, 'jsx', (node) => {
-        const matches = node.value.match(/^(<img\s.*?src=)"(\s*\/.*?)"(.*)$/);
+        const matches = node.value.match(/^(<img\s.*?src="\s*)\/(.*)$/);
         if (matches != null) {
-          node.value = `${matches[1]}{require("${matches[2]}").default}${matches[3]}`;
+          node.value = `${matches[1]}${baseUrl}${matches[2]}`;
         }
       });
     };
