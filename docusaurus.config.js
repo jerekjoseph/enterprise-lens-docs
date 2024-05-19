ffd8c958904f3a89f57d5f6bdda39445e914068b
MCN <60007795+jerekjoseph@users.noreply.github.com>
2024-05-19 16:28:09 +0800

    Remove old plugin import

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -5,7 +5,7 @@
 // See: https://docusaurus.io/docs/api/docusaurus-config
 
 import {themes as prismThemes} from 'prism-react-renderer';
-import remarkImageSrcWithBase from './src/remark/imagesSrcWithBase.js';
+
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Documentation',
