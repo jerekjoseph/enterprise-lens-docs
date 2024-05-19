e3d9452e2f96fc1823092b0c3c4df315f1148c48
MCN <60007795+jerekjoseph@users.noreply.github.com>
2024-05-19 15:55:36 +0800

    Utilise only ver 2. of plugin

--- src/remark/imagesSrcWithBase.js
+++ src/remark/imagesSrcWithBase.js
@@ -1,20 +1,3 @@
-/**
- * Docusaurus does not process JSX `<img src ="...">` URLs
- * This plugin rewrites the URLs to be relative to `baseUrl`
- * Markdown links `[]()`, images `![](/image)` and anchor `<a href="...">`
- * are already automatically processed
- */
-const remarkImageSrcWithBase = (options) => {
-    return (ast) => {
-      visit(ast, 'jsx', (node) => {
-        const matches = node.value.match(/^(<img\s.*?src="\s*)\/(.*)$/);
-        if (matches != null) {
-          node.value = `${matches[1]}${baseUrl}${matches[2]}`;
-        }
-      });
-    };
-  };
-  
   /**
    * Docusaurus does not process JSX `<img src ="...">` URLs
    * This plugin rewrites the src attribute to `src={require("...").default}`
